903aa88df9755dbffe3457be4b5325dd6afbf915
Dylan Copeland <baddylan123@gmail.com>
2023-05-09 12:26:55 -0500

    Updated logic of the scripts slightly.

--- install-part2.sh
+++ install-part2.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-echo "Installing nix..."
-sh <(curl -L https://nixos.org/nix/install) --daemon
+function beginInstall {
+	if [$startNow == "n" ] || [$startNow == "no" ] || [$startNow == "N" ] || [$startNow == "N" ]
+	then
+		echo "Aborting..."
+		exit 0
+	else
+		echo "Installing Nix..."
+		sh <(curl -L https://nixos.org/nix/install) --daemon
+	fi
+}
+
+
+
+
+echo "Ready to install Nix Package Manager? [y/n]"
+read startNow
--- install-part3.sh
+++ install-part3.sh
@@ -3,4 +3,4 @@
 echo "Changing Swappiness 100 to 10 (Reduces use of Swap file)"
 echo "vm.swappiness=10" | sudo tee /etc/sysctl.d/99-custom-swappiness.conf
 
-echo "Increase swap file aue if you want to."
+echo "Increase Swap file size if you want to."
